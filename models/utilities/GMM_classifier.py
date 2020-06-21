5afecfbebb43c2d81c9253d83470b20424fef1df
katerinavr <43647831+katerinavr@users.noreply.github.com>
2020-06-21 21:57:54 +0100

    Add files via upload

---
+++ models/utilities/GMM_classifier.py
@@ -0,0 +1,25 @@
+from sklearn.mixture import GaussianMixture
+
+"""
+Builds the Gaussian Mixture Classifier to be used for anomaly detection
+When points are scored under a pre-specified threshold, they are regarged as outliers
+
+"""
+contamination = 0.05
+
+class GMM(GaussianMixture):
+  def __init__(self, n_components, covariance_type, random_state):
+    super().__init__(n_components=n_components , covariance_type=covariance_type, random_state=random_state)
+
+  def fit(self, X, y):
+    super().fit(X, y)
+    self.prob = super().score_samples(X_train)
+    self.c = percentile(self.prob, 100 * contamination)
+
+  def predict(self, X):
+    scores = []
+    proba=super().score_samples(X)
+    
+    scores =(proba <= self.c).astype('int').ravel()
+
+    return scores
